b9742ea626b2e33b4a1091471c08d8eeceff8588
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2018-12-29 12:11:31 -0300

    Import de collapse do bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import { Collapse } from 'bootstrap-vue/es/components'
 
 Vue.config.productionTip = false
+
+Vue.use(Collapse)
 
 new Vue({
   store,
