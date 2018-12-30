959c749f1a461c3b89ff6b10b2fd693dc9d715ce
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2018-12-30 18:56:59 -0300

    Ajustes no HeaderMain

--- src/components/Header/HeaderMain.vue
+++ src/components/Header/HeaderMain.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="HeaderMain">
-    .
+    <MenuToggleTelefone />
     <MenuToggleMain />
+    <div class="w-100"></div>
+    <LogoGrandMotors />
+    <LogoToyota />
   </div>
 </template>
 
 <script>
 import MenuToggleMain from '@/components/Menu/MenuToggleMain.container'
+import MenuToggleTelefone from '@/components/Menu/MenuToggleTelefone'
+import LogoGrandMotors from '@/components/Header/LogoGrandMotors'
+import LogoToyota from '@/components/Header/LogoToyota'
 
 export default {
   name: 'HeaderMain',
 
   components: {
-    MenuToggleMain
+    MenuToggleMain,
+    MenuToggleTelefone,
+    LogoGrandMotors,
+    LogoToyota
   }
 }
 </script>
@@ -21,6 +30,8 @@ export default {
 .HeaderMain {
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
   padding: 20px 0;
   position: relative;
 }
