9c0f33d9a75b4f22011e5b45e6c92d0c7f021cc7
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2018-12-30 18:54:14 -0300

    Adicionado comps de Logos

---
+++ src/components/Header/LogoGrandMotors.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img :src="ImgLogo" alt="Grand Motors">
+  </div>
+</template>
+
+<script>
+import ImgLogo from '@/assets/images/logo_Logo-Grand-Motors.png'
+
+export default {
+  name: 'LogoGrandMotors',
+
+  data () {
+    return {
+      ImgLogo
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header/LogoToyota.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img :src="ImgLogo" alt="Toyota">
+  </div>
+</template>
+
+<script>
+import ImgLogo from '@/assets/images/logo_brand_horizontal_10.png'
+
+export default {
+  name: 'LogoToyta',
+
+  data () {
+    return {
+      ImgLogo
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
