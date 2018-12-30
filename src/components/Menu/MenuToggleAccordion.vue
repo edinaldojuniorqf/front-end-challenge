7cf93863a9e22fd23a64a6fb0784f2fd3398ee7d
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2018-12-30 14:58:58 -0300

    Injeção de componentes no MenuAccordion

---
+++ src/components/Menu/MenuToggleAccordion.vue
@@ -0,0 +1,15 @@
+<template>
+  <MenuToggle>
+    <MenuAccordion :items="items" />
+  </MenuToggle>
+</template>
+
+<script>
+export default {
+  name: 'MenuToggleAccordion'
+}
+</script>
+
+<style>
+
+</style>
