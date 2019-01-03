0a2af6fd29bb634a92203ef674dcce5f87f3086f
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2019-01-03 11:10:10 -0300

    Adicionado simple-vue-validator

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import { Collapse } from 'bootstrap-vue/es/components'
 import vSelect from 'vue-select'
 import VueTheMask from 'vue-the-mask'
 import money from 'v-money'
+import SimpleVueValidation from 'simple-vue-validator'
 
 Vue.config.productionTip = false
 
 Vue.use(Collapse)
 Vue.use(VueTheMask)
 Vue.use(money, {precision: 2})
+Vue.use(SimpleVueValidation)
 Vue.component('v-select', vSelect)
 
 new Vue({
