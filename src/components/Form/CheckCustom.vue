c23dae5c9a307c8813b7d408834a33470f96e064
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2019-01-03 11:03:03 -0300

    Radio e checkbox personalizado

---
+++ src/components/Form/CheckCustom.vue
@@ -0,0 +1,91 @@
+<template>
+  <label class="CheckCustom">
+    <input type="checkbox" :name="name" :value="value" @input="handleInput">
+    <span class="checkmark"></span>
+    <slot />
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'CheckCustom',
+
+  props: {
+    value: {
+      type: Boolean
+    },
+
+    name: {
+      type: String
+    }
+  },
+
+  methods: {
+    handleInput (e) {
+      this.$emit('input', e.target.checked)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.CheckCustom {
+  display: block;
+  position: relative;
+  margin-bottom: 12px;
+  cursor: pointer;
+  user-select: none;
+  display: flex;
+}
+
+/* Hide the browser's default checkbox */
+.CheckCustom input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* Create a custom checkbox */
+.CheckCustom .checkmark {
+  display: block;
+  position: relative;
+  height: 16px;
+  width: 16px;
+  border: 2px solid $color-white;
+  border-radius: $border-radius;
+  margin-right: $margin-tiny;
+}
+
+/* On mouse-over, add a grey background color */
+.CheckCustom:hover input ~ .checkmark {
+  /* background-color: #ccc; */
+}
+
+/* When the checkbox is checked, add a blue background */
+.CheckCustom input:checked ~ .checkmark {
+  /* background-color: $color-white; */
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.CheckCustom .checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.CheckCustom input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the checkmark/indicator */
+.CheckCustom .checkmark:after {
+  left: 2px;
+  top: 2px;
+  width: 8px;
+  height: 8px;
+  background: $color-white;
+}
+</style>
---
+++ src/components/Form/RadioCustom.vue
@@ -0,0 +1,97 @@
+<template>
+  <label class="RadioCustom">
+    <input type="radio"
+      :name="name"
+      :value="valueDefalut"
+      @input="handleInput">
+    <span class="checkmark"></span>
+    <slot />
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'RadioCustom',
+
+  props: {
+    value: {
+      type: String
+    },
+
+    valueDefalut: {
+      type: String
+    },
+
+    name: {
+      type: String
+    }
+  },
+
+  methods: {
+    handleInput (e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.RadioCustom {
+  display: flex;
+  align-items: center;
+  position: relative;
+  margin-bottom: $margin-tiny;
+  cursor: pointer;
+  user-select: none;
+}
+
+/* Hide the browser's default radio button */
+.RadioCustom input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+}
+
+/* Create a custom radio button */
+.RadioCustom .checkmark {
+  display: block;
+  position: relative;
+  height: 16px;
+  width: 16px;
+  border: 2px solid $color-white;
+  border-radius: 50%;
+  margin-right: $margin-tiny;
+}
+
+/* On mouse-over, add a grey background color */
+.RadioCustom:hover input ~ .checkmark {
+  /* background-color: #ccc; */
+}
+
+/* When the radio button is checked, add a blue background */
+.RadioCustom input:checked ~ .checkmark {
+  /* background-color: #2196F3; */
+}
+
+/* Create the indicator (the dot/circle - hidden when not checked) */
+.RadioCustom .checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the indicator (dot/circle) when checked */
+.RadioCustom input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the indicator (dot/circle) */
+.RadioCustom .checkmark:after {
+  top: 2px;
+  left: 2px;
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  background: white;
+}
+</style>
