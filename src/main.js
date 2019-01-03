b1517fe8a19da07b8d150413234ce03465994c7a
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2019-01-03 11:00:22 -0300

    Adicionado vue-select, vue-the-mask e v-money

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import { Collapse } from 'bootstrap-vue/es/components'
+import vSelect from 'vue-select'
+import VueTheMask from 'vue-the-mask'
+import money from 'v-money'
 
 Vue.config.productionTip = false
 
 Vue.use(Collapse)
+Vue.use(VueTheMask)
+Vue.use(money, {precision: 2})
+Vue.component('v-select', vSelect)
 
 new Vue({
   store,
