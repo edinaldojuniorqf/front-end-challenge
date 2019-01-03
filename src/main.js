defed879cdd8475a6afe3392650d578215b059dd
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2019-01-03 20:21:53 -0300

    Seta veículo ao iniciar carremento da página

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.container'
 import store from './store'
 import { Collapse } from 'bootstrap-vue/es/components'
 import vSelect from 'vue-select'
