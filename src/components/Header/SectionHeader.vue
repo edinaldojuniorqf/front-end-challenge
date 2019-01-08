8eb65e75aa0f062d7dd358fbb5df5682156c6515
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2019-01-08 12:51:53 -0300

    Vídeo não exibido em telas pequenas

--- src/components/Header/SectionHeader.vue
+++ src/components/Header/SectionHeader.vue
@@ -1,45 +1,69 @@
 <template>
-  <div class="SectionHeader">
-    <VideoFull>
+  <div class="SectionHeader"
+    :class="{
+      'SectionHeader--backgroundImg': isMobile === true
+    }">
+    <VideoFull v-if="isMobile === false">
       <iframe slot="video" src="https://player.vimeo.com/video/202373809?autoplay=1&loop=1&autopause=0&title=0&byline=0&portrait=0&muted=1&background=1" allow="autoplay" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-      <div class="container">
-        <HeaderMain />
-      </div>
-      <div class="container SectionHeader__container">
-        <div class="row">
-          <div class="d-flex col-md-8 order-md-2">
-            <HeaderContent />
-          </div>
-          <div class="col-md-4 order-md-1">
-            <FormSimulacao />
-          </div>
-        </div>
-      </div>
+      <SectionHeaderContent />
     </VideoFull>
+    <SectionHeaderContent v-else-if="isMobile === true" />
   </div>
 </template>
 
 <script>
-import HeaderMain from '@/components/Header/HeaderMain'
 import VideoFull from '@/components/Header/VideoFull'
-import HeaderContent from '@/components/Header/HeaderContent.container'
-import FormSimulacao from '@/components/Form/FormSimulacao.container'
+import SectionHeaderContent from '@/components/Header/SectionHeaderContent'
 
 export default {
   name: 'SectionHeader',
 
   components: {
-    HeaderMain,
     VideoFull,
-    HeaderContent,
-    FormSimulacao
+    SectionHeaderContent
+  },
+
+  data () {
+    return {
+      isMobile: null
+    }
+  },
+
+  methods: {
+    setBackground () {
+      if (window.matchMedia(`(max-width: ${this.$style.sm})`).matches) {
+        this.isMobile = true
+      } else {
+        this.isMobile = false
+      }
+    }
+  },
+
+  mounted () {
+    this.setBackground()
+    window.addEventListener('resize', () => {
+      this.setBackground()
+    })
   }
 }
 </script>
 
-<style lang="scss">
-.SectionHeader__container {
-  margin-top: $margin-big;
-  padding-bottom: $padding-extra;
+<style lang="scss" module>
+:export {
+  @each $breakpoint, $size in $grid-breakpoints {
+    #{$breakpoint}: $size;
+  }
+}
+
+.SectionHeader {
+  .SectionHeader__container {
+    margin-top: $margin-big;
+    padding-bottom: $padding-extra;
+  }
+}
+
+.SectionHeader--backgroundImg {
+  background: url('../../assets/images/bg-video.png');
+  background-size: cover;
 }
 </style>
---
+++ src/components/Header/SectionHeaderContent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="SectionHeaderContent">
+    <div class="container">
+      <HeaderMain />
+    </div>
+    <div class="container SectionHeader__container">
+      <div class="row">
+        <div class="d-flex col-md-8 order-md-2">
+          <HeaderContent />
+        </div>
+        <div class="col-md-4 order-md-1">
+          <FormSimulacao />
+        </div>
+      </div>
+    </div>
+  </div>  
+</template>
+
+<script>
+import HeaderMain from '@/components/Header/HeaderMain'
+import HeaderContent from '@/components/Header/HeaderContent.container'
+import FormSimulacao from '@/components/Form/FormSimulacao.container'
+
+export default {
+  name: 'SectionHeaderContent',
+
+  components: {
+    HeaderMain,
+    HeaderContent,
+    FormSimulacao
+  }
+}
+</script>
+
+<style>
+
+</style>
