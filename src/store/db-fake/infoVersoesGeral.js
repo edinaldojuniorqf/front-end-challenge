01a3d55d9be66721b2b492635e9eebb67abf40d2
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2019-01-08 11:39:51 -0300

    Adicionado dados fake infoVersoesGeral

--- src/store/db-fake/infoVersoesGeral.js
+++ src/store/db-fake/infoVersoesGeral.js
@@ -1,8 +1,49 @@
 export default [
   {
     versao: {
+      id: 1,
+      nome: 'X PLUS MAN',
+    },
+    imgDestaque: 'http://localhost:8080/images/versoes/carro.png',
+    infoGeral: [
+      {
+        key: 'Cilindro/válvulas por cinlindro',
+        value: '4/4'
+      },
+      {
+        key: 'Diâmetro/curso (mm)',
+        value: '97.7/10.91'
+      },
+      {
+        key: 'Cilindrada (cc)',
+        value: '2,998'
+      },
+      {
+        key: 'Potência máxima EEC - Cv',
+        value: '540'
+      },
+      {
+        key: 'em rev/min',
+        value: '5,500'
+      },
+      {
+        key: 'Torque máximo EEC - Nm (libra-pré)',
+        value: '340'
+      },
+      {
+        key: 'em rev/min',
+        value: '2,000-4000'
+      }
+    ],
+    tags: [
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro e Dianteiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar'
+    ]
+  },
+  {
+    versao: {
       id: 2,
-      nome: '',
+      nome: 'X PLUS MAN',
     },
     imgDestaque: 'http://localhost:8080/images/versoes/carro.png',
     infoGeral: [
@@ -42,8 +83,49 @@ export default [
   },
   {
     versao: {
-      id: 1,
-      nome: 'X PLUS MAN',
+      id: 3,
+      nome: 'XLS 1.5 AT',
+    },
+    imgDestaque: 'http://localhost:8080/images/versoes/carro.png',
+    infoGeral: [
+      {
+        key: 'Cilindro/válvulas por cinlindro',
+        value: '2/4'
+      },
+      {
+        key: 'Diâmetro/curso (mm)',
+        value: '18.5/8.31'
+      },
+      {
+        key: 'Cilindrada (cc)',
+        value: '3,999'
+      },
+      {
+        key: 'Potência máxima EEC - Cv',
+        value: '120'
+      },
+      {
+        key: 'em rev/min',
+        value: '4,500'
+      },
+      {
+        key: 'Torque máximo EEC - Nm (libra-pré)',
+        value: '300'
+      },
+      {
+        key: 'em rev/min',
+        value: '2,000-4000'
+      }
+    ],
+    tags: [
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED'
+    ]
+  },
+  {
+    versao: {
+      id: 4,
+      nome: 'X 1.5 AT',
     },
     imgDestaque: 'http://localhost:8080/images/versoes/carro.png',
     infoGeral: [
@@ -53,23 +135,68 @@ export default [
       },
       {
         key: 'Diâmetro/curso (mm)',
-        value: '97.7/10.91'
+        value: '28.5/8.31'
       },
       {
         key: 'Cilindrada (cc)',
-        value: '2,998'
+        value: '4,299'
       },
       {
         key: 'Potência máxima EEC - Cv',
-        value: '540'
+        value: '410'
       },
       {
         key: 'em rev/min',
-        value: '5,500'
+        value: '6,500'
       },
       {
         key: 'Torque máximo EEC - Nm (libra-pré)',
-        value: '340'
+        value: '400'
+      },
+      {
+        key: 'em rev/min',
+        value: '1,000-4000'
+      }
+    ],
+    tags: [
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED'
+    ]
+  },
+  {
+    versao: {
+      id: 5,
+      nome: 'X-STD 1.5',
+    },
+    imgDestaque: 'http://localhost:8080/images/versoes/carro.png',
+    infoGeral: [
+      {
+        key: 'Cilindro/válvulas por cinlindro',
+        value: '4/4'
+      },
+      {
+        key: 'Diâmetro/curso (mm)',
+        value: '26.5/9.11'
+      },
+      {
+        key: 'Cilindrada (cc)',
+        value: '4,399'
+      },
+      {
+        key: 'Potência máxima EEC - Cv',
+        value: '610'
+      },
+      {
+        key: 'em rev/min',
+        value: '8,500'
+      },
+      {
+        key: 'Torque máximo EEC - Nm (libra-pré)',
+        value: '500'
       },
       {
         key: 'em rev/min',
@@ -77,8 +204,325 @@ export default [
       }
     ],
     tags: [
-      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro e Dianteiro', 'Ar quente',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente'
+    ]
+  },
+  {
+    versao: {
+      id: 6,
+      nome: '2.0L 4x2 CVT',
+    },
+    imgDestaque: 'http://localhost:8080/images/versoes/carro.png',
+    infoGeral: [
+      {
+        key: 'Cilindro/válvulas por cinlindro',
+        value: '4/4'
+      },
+      {
+        key: 'Diâmetro/curso (mm)',
+        value: '25.5/8.31'
+      },
+      {
+        key: 'Cilindrada (cc)',
+        value: '4,299'
+      },
+      {
+        key: 'Potência máxima EEC - Cv',
+        value: '610'
+      },
+      {
+        key: 'em rev/min',
+        value: '4,500'
+      },
+      {
+        key: 'Torque máximo EEC - Nm (libra-pré)',
+        value: '300'
+      },
+      {
+        key: 'em rev/min',
+        value: '2,000-4000'
+      }
+    ],
+    tags: [
+      'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED'
+    ]
+  },
+  {
+    versao: {
+      id: 7,
+      nome: '2.0L 4x2 CVT TOP',
+    },
+    imgDestaque: 'http://localhost:8080/images/versoes/carro.png',
+    infoGeral: [
+      {
+        key: 'Cilindro/válvulas por cinlindro',
+        value: '4/4'
+      },
+      {
+        key: 'Diâmetro/curso (mm)',
+        value: '48.5/2.31'
+      },
+      {
+        key: 'Cilindrada (cc)',
+        value: '2,299'
+      },
+      {
+        key: 'Potência máxima EEC - Cv',
+        value: '230'
+      },
+      {
+        key: 'em rev/min',
+        value: '3,500'
+      },
+      {
+        key: 'Torque máximo EEC - Nm (libra-pré)',
+        value: '200'
+      },
+      {
+        key: 'em rev/min',
+        value: '3,000-4000'
+      }
+    ],
+    tags: [
+      'Ar condicionado', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
       'Freios ABS', 'Teto Solar'
+    ]
+  },
+  {
+    versao: {
+      id: 8,
+      nome: '1.8L DUAL VVT-l 16V',
+    },
+    imgDestaque: 'http://localhost:8080/images/versoes/carro.png',
+    infoGeral: [
+      {
+        key: 'Cilindro/válvulas por cinlindro',
+        value: '4/4'
+      },
+      {
+        key: 'Diâmetro/curso (mm)',
+        value: '8.5/8.31'
+      },
+      {
+        key: 'Cilindrada (cc)',
+        value: '2,199'
+      },
+      {
+        key: 'Potência máxima EEC - Cv',
+        value: '310'
+      },
+      {
+        key: 'em rev/min',
+        value: '2,500'
+      },
+      {
+        key: 'Torque máximo EEC - Nm (libra-pré)',
+        value: '300'
+      },
+      {
+        key: 'em rev/min',
+        value: '1,000-4000'
+      }
+    ],
+    tags: [
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED'
+    ]
+  },
+  {
+    versao: {
+      id: 9,
+      nome: '2.0L DUAL VVT-l 16V',
+    },
+    imgDestaque: 'http://localhost:8080/images/versoes/carro.png',
+    infoGeral: [
+      {
+        key: 'Cilindro/válvulas por cinlindro',
+        value: '2/4'
+      },
+      {
+        key: 'Diâmetro/curso (mm)',
+        value: '18.5/4.31'
+      },
+      {
+        key: 'Cilindrada (cc)',
+        value: '6,399'
+      },
+      {
+        key: 'Potência máxima EEC - Cv',
+        value: '310'
+      },
+      {
+        key: 'em rev/min',
+        value: '4,500'
+      },
+      {
+        key: 'Torque máximo EEC - Nm (libra-pré)',
+        value: '330'
+      },
+      {
+        key: 'em rev/min',
+        value: '3,000-4000'
+      }
+    ],
+    tags: [
+      'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED'
+    ]
+  },
+  {
+    versao: {
+      id: 10,
+      nome: 'SR 4x2 2.7 Flex MT',
+    },
+    imgDestaque: 'http://localhost:8080/images/versoes/carro.png',
+    infoGeral: [
+      {
+        key: 'Cilindro/válvulas por cinlindro',
+        value: '4/4'
+      },
+      {
+        key: 'Diâmetro/curso (mm)',
+        value: '48.2/18.31'
+      },
+      {
+        key: 'Cilindrada (cc)',
+        value: '3,299'
+      },
+      {
+        key: 'Potência máxima EEC - Cv',
+        value: '413'
+      },
+      {
+        key: 'em rev/min',
+        value: '8,500'
+      },
+      {
+        key: 'Torque máximo EEC - Nm (libra-pré)',
+        value: '400'
+      },
+      {
+        key: 'em rev/min',
+        value: '3,000-4000'
+      }
+    ],
+    tags: [
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED'
+    ]
+  },
+  {
+    versao: {
+      id: 11,
+      nome: 'SR 4x2 2.7 Flex AT',
+    },
+    imgDestaque: 'http://localhost:8080/images/versoes/carro.png',
+    infoGeral: [
+      {
+        key: 'Cilindro/válvulas por cinlindro',
+        value: '4/4'
+      },
+      {
+        key: 'Diâmetro/curso (mm)',
+        value: '22.5/9.31'
+      },
+      {
+        key: 'Cilindrada (cc)',
+        value: '5,779'
+      },
+      {
+        key: 'Potência máxima EEC - Cv',
+        value: '450'
+      },
+      {
+        key: 'em rev/min',
+        value: '2,500'
+      },
+      {
+        key: 'Torque máximo EEC - Nm (libra-pré)',
+        value: '400'
+      },
+      {
+        key: 'em rev/min',
+        value: '1,000-4000'
+      }
+    ],
+    tags: [
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED'
+    ]
+  },
+  {
+    versao: {
+      id: 12,
+      nome: 'SR 4x2 2.7 Turbo Diesel',
+    },
+    imgDestaque: 'http://localhost:8080/images/versoes/carro.png',
+    infoGeral: [
+      {
+        key: 'Cilindro/válvulas por cinlindro',
+        value: '2/4'
+      },
+      {
+        key: 'Diâmetro/curso (mm)',
+        value: '16.8/5.21'
+      },
+      {
+        key: 'Cilindrada (cc)',
+        value: '5,849'
+      },
+      {
+        key: 'Potência máxima EEC - Cv',
+        value: '900'
+      },
+      {
+        key: 'em rev/min',
+        value: '4,500'
+      },
+      {
+        key: 'Torque máximo EEC - Nm (libra-pré)',
+        value: '900'
+      },
+      {
+        key: 'em rev/min',
+        value: '3,000-4000'
+      }
+    ],
+    tags: [
+      'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Freios ABS', 'Teto Solar', 'Farol em LED',
+      'Ar condicionado', 'AirBag', 'Desembaçador Traseiro', 'Ar quente',
+      'Freios ABS', 'Teto Solar', 'Farol em LED'
     ]
   }
 ]
