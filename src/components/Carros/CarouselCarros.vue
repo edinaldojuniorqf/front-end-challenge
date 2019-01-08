23f04b56d00b29c8f5b5b8a45310624d21ec61d2
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2019-01-08 13:22:48 -0300

    Adicionado sessão carros

---
+++ src/components/Carros/CarouselCarros.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="CarouselCarros">
+    <carousel
+      :per-page="perPage"
+      :navigationEnabled="true"
+      :navigationClickTargetSize="1"
+      navigationNextLabel='>'
+      navigationPrevLabel='<'
+      paginationActiveColor="#b71d3e"
+      paginationColor="#c6c6c6"
+      :paginationSize="10">
+      <slide>
+        <img class="img-fluid" src="http://localhost:8080/images/carros/carro-classe-a.png" alt="Classe A">
+        <h5 class="CarouselCarros__title">
+          Classe A
+        </h5>
+      </slide>
+
+      <slide>
+        <img class="img-fluid" src="http://localhost:8080/images/carros/carro-classe-a.png" alt="Classe A">
+        <h5 class="CarouselCarros__title">
+          Classe A
+        </h5>
+      </slide>
+
+      <slide>
+        <img class="img-fluid" src="http://localhost:8080/images/carros/carro-classe-a.png" alt="Classe A">
+        <h5 class="CarouselCarros__title">
+          Classe A
+        </h5>
+      </slide>
+
+      <slide>
+        <img class="img-fluid" src="http://localhost:8080/images/carros/carro-classe-a.png" alt="Classe A">
+        <h5 class="CarouselCarros__title">
+          Classe A
+        </h5>
+      </slide>
+
+      <slide>
+        <img class="img-fluid" src="http://localhost:8080/images/carros/carro-classe-a.png" alt="Classe A">
+        <h5 class="CarouselCarros__title">
+          Classe A
+        </h5>
+      </slide>
+
+      <slide>
+        <img class="img-fluid" src="http://localhost:8080/images/carros/carro-classe-a.png" alt="Classe A">
+        <h5 class="CarouselCarros__title">
+          Classe A
+        </h5>
+      </slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CarouselCarros',
+
+  props: {
+    perPage: {
+      type: Number,
+      default: 5
+    }
+  },
+
+  mounted () {
+  }
+}
+</script>
+
+<style lang="scss">
+.CarouselCarros {
+
+  .CarouselCarros__title {
+    font-weight: $font-weight-normal;
+    $color: $color-gray-darker;
+  }
+
+  .VueCarousel-slide {
+    text-align: center;
+  }
+
+  .VueCarousel-dot-container {
+    margin-top: 0 !important;
+  }
+
+  .VueCarousel-navigation-button {
+    background: rgba(0, 0, 0, .35) !important;
+    color: $color-white !important;
+    padding: 5px 10px !important;
+    font-size: $font-size-wee;
+  }
+}
+</style>
---
+++ src/components/Carros/SectionCarros.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="SectionCarros">
+    <div class="container">
+      <h3 class="SectionCarros__title">Conheça nossos carros</h3>
+      <CarouselCarros />
+    </div>
+  </div>
+</template>
+
+<script>
+import CarouselCarros from '@/components/Carros/CarouselCarros'
+
+export default {
+  name: 'SectionCarros',
+
+  components: {
+    CarouselCarros
+  }
+}
+</script>
+
+<style lang="scss">
+.SectionCarros {
+  background: $color-white;  
+  padding: $padding-big 0;
+  color: $color-gray-darker;
+
+  .SectionCarros__title {
+    font-size: 1rem * (32 / $font-size-base);
+    font-weight: $font-weight-light;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: $margin-big;
+
+    &::after {
+      content: '';
+      background: rgba(55, 52, 53, .3);
+      width: 100px;
+      height: 1px;
+      margin-top: $margin-small;
+    }
+  }
+}
+</style>
