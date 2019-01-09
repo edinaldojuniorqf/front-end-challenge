a2be63d96bf348b69f1b105f9d9bf160bab1245d
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2019-01-09 11:59:37 -0300

    Correção na exibição da troca de imagem e vídeo de background

--- src/components/Header/SectionHeader.vue
+++ src/components/Header/SectionHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="SectionHeader"
     :class="{
-      'SectionHeader--backgroundImg': isMobile === true
+      [$style['SectionHeader--backgroundImg']]: isMobile === true
     }">
     <VideoFull v-if="isMobile === false">
       <iframe slot="video" src="https://player.vimeo.com/video/202373809?autoplay=1&loop=1&autopause=0&title=0&byline=0&portrait=0&muted=1&background=1" allow="autoplay" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
